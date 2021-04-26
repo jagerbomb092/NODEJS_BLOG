a485ac169e43c92711715ef5c547c028a6f7e690
Đinh Tuấn Nghĩa <dinhtuannghia35@gmail.com>
2021-04-26 23:32:00 +0700

    change

--- src/main.js
+++ src/main.js
@@ -1,29 +1,47 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const handlebars = require('express-handlebars');
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 //HTTp
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 // Logger
-app.engine('hbs', handlebars({
-  extname:'.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 // console.log(__dirname);
 
 app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+    res.render('home');
+});
+app.get('/movie', (req, res) => {
+    res.render('movie');
+});
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.send('');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
